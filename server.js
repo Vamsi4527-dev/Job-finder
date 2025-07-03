e572601346a5156cf8bed7e7aeb6977d91cde0f3
Vamsi Krishna Dasari <vamsikrishnadasari1234@gmail.com>
2025-07-03 22:36:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,23 +2,11 @@ const express = require("express");
 const path = require("path");
 const admin = require("firebase-admin");
 
-// Use environment variables instead of requiring JSON file
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
-};
+const serviceAccount = require("./job-finder-cbf74-firebase-adminsdk-fbsvc-b9e31fb930.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL || "https://job-finder-cbf74-default-rtdb.firebaseio.com"
+  databaseURL: "https://job-finder-cbf74-default-rtdb.firebaseio.com"
 });
 
 const db = admin.firestore();
@@ -27,6 +15,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -40,9 +29,11 @@ app.get("/signup", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "signup.html"));
 });
 
+
 app.post("/api/signup", async (req, res) => {
     try {
         const { name, email, password } = req.body;
+        
         
         const userRecord = await admin.auth().createUser({
             email: email,
@@ -50,6 +41,7 @@ app.post("/api/signup", async (req, res) => {
             displayName: name,
         });
 
+       
         await db.collection('users').doc(userRecord.uid).set({
             name: name,
             email: email,
@@ -105,6 +97,7 @@ app.post("/api/login", async (req, res) => {
     }
 });
 
+
 app.get("/api/users", async (req, res) => {
     try {
         const snapshot = await db.collection('users').get();
@@ -118,7 +111,7 @@ app.get("/api/users", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
